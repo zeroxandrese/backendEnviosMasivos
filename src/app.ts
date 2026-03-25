49fbbe313702b42ac54faa53d75051119a5f8798
Andres Ernesto Lopez Gutierrez <alopezg@allmark.com>
2026-03-25 16:35:45 -0500

    backend EnviosMasivos v1.0.9

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://frontend-enviosmasivos.onrender.com"
+    "https://frontend-envios-masivos.vercel.app"
   ],
   credentials: true
 }));
